61138dc13700079c61f51328bbcbcef738f07816
zhoua_yan <zhoua_yan@163.com>
2015-07-21 22:56:51 +0800

    The current progress.Seperate home and myFile.

--- js/qile.js
+++ js/qile.js
@@ -1,2 +1,16 @@
-// JavaScript Document
+$("document").ready(function(){
+	$("#item").mouseover(function(){
+		$("#item .dropdown-menu").show();
+	});
+	$("#item").mouseout(function(){
+		$("#item .dropdown-menu").hide();
+	});
+	$("#more").mouseover(function(){
+		$("#more .dropdown-menu").show();
+	});
+	$("#more").mouseout(function(){
+		$("#more .dropdown-menu").hide();
+	});
+
+})
 
